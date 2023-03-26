7fdd7dc9d5ddd17a05bbe09f29fbba6dd9f2b882
Talles Borges <talles.borges92@gmail.com>
2023-03-26 13:05:32 -0300

    improvements

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,39 @@ import { ref } from 'vue';
 import HelloWorld from './components/HelloWorld.vue'
 
 const showApiKey = () => {
-  isVisable.value = !isVisable.value;
+  isApiVisible.value = !isApiVisible.value;
+}
+const showPrompot = () => {
+  isPrompotVisible.value = !isPrompotVisible.value;
 }
 
 const getApiKey = () => {
   return localStorage.getItem('apiKey')?? '';
 }
+
+const defaultPrompot = `Correct the spelling, grammar, and clarity issues in the given text and output the corrected text in JSON format with only the key 'corrected' and an array of explanations for the corrections made. Ensure that the JSON returned is valid and can be parsed by a JSON parser.
+Example output: {"corrected": "The quick brown fox jumps over the lazy dog.", "explanations": ["The word 'jumps' was corrected to 'jump' because it is the correct conjugation of the verb 'jump'"]}
+Text: "###text###"
+Output:`;
+
+const getPrompt = () => {
+  return localStorage.getItem('prompt') || defaultPrompot;
+}
+
 const apiKey = ref(getApiKey());
-const isVisable = ref(getApiKey() === '');
+const prompt = ref(getPrompt());
+const isApiVisible = ref(getApiKey() === '');
+const isPrompotVisible = ref(false);
 
 const saveApiKey = () => {
-  isVisable.value = false;
+  isApiVisible.value = false;
   localStorage.setItem('apiKey', apiKey.value);
+  window.location.reload();
+}
+
+const savePrompt = () => {
+  isPrompotVisible.value = false;
+  localStorage.setItem('prompt', prompt.value);
   window.location.reload();
 }
 </script>
@@ -22,16 +43,19 @@ const saveApiKey = () => {
 <template>
   <div>
     <div class="container mx-auto">
-      <input v-show="isVisable" v-model="apiKey" type="text" class="w-[480px] p-2 border-2 border-gray-300 rounded-lg"
+      <input v-show="isApiVisible" v-model="apiKey" type="text" class="w-[480px] p-2 border-2 border-gray-300 rounded-lg"
         placeholder="API KEY" />
+        <textarea v-show="isPrompotVisible" v-model="prompt" type="text" class="w-full h-36 p-2 border-2 border-gray-300 rounded-lg"/>        
       <div class="flex space-x-2">
-        <a class="text-blue-500 cursor-pointer underline" @click="showApiKey">{{ isVisable ? 'Hide' : 'Show API' }}</a>
+        <a class="text-blue-500 cursor-pointer underline" @click="showApiKey">{{ isApiVisible ? 'Hide API' : 'Show API' }}</a>
+        <a v-if="isApiVisible" class="text-blue-500 cursor-pointer underline" @click="saveApiKey">Save API</a>
         <a href="https://platform.openai.com/account/api-keys" target="_blank" class="text-blue-500 cursor-pointer underline">Manage Keys</a>
-        <a v-if="isVisable" class="text-blue-500 cursor-pointer underline" @click="saveApiKey">Save</a>
+        <a class="text-blue-500 cursor-pointer underline" @click="showPrompot">{{ isPrompotVisible ? 'Hide Prompot' : 'Show Prompot' }}</a>
+        <a v-if="isPrompotVisible" class="text-blue-500 cursor-pointer underline" @click="savePrompt">Save Prompt</a>
       </div>
     </div>
     <div class="flex justify-center items-center min-h-screen">
-      <HelloWorld v-if="getApiKey() !== ''" :api-key="getApiKey()" class="container mx-auto" />
+      <HelloWorld v-if="getApiKey() !== ''" :api-key="getApiKey()" :prompt="getPrompt()" class="container mx-auto" />
     </div>
   </div>
 </template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,3 +1,51 @@
+<template>
+  <div>
+    <div class="flex flex-col max-w-4xl mx-auto">
+      <a class="text-blue-500 cursor-pointer" @click="reset">Clear</a>
+      <!-- temperature -->
+      <div class="flex flex-row space-x-2">
+        <span >Temperature</span>
+        <input type="range" min="0" max="2" step="0.01" v-model="temperature" />
+        <span v-text="temperature" />
+      </div>
+      <!-- top_p -->
+      <div class="flex flex-row space-x-2">
+        <span >Top P</span>
+        <input type="range" min="0" max="1" step="0.01" v-model="top_p" />
+        <span v-text="top_p" />
+      </div>
+      <!-- prompt -->
+      <div class="flex flex-row space-x-2">
+        <textarea class="border rounded w-full min-h-[160px] p-2" v-model="text" />
+        <div class="border rounded w-full bg-gray-100 p-2 overflow-y-scroll min-h-full">
+          <span v-text="responseJson['corrected']" />
+          <ul class="list-disc list-inside">
+            <li v-for="explanation in responseJson['explanations']" v-text="explanation" />
+          </ul>
+        </div>
+      </div>
+      <br>
+      <button class="btn btn-blue" type="button" @click="send">Process</button>
+      <br>
+      <br>
+      <span class="text-2xl font-extrabold">History</span>
+      <div class="flex flex-col space-y-2">
+        <div v-for="item in history" class="flex items-center space-x-2">
+          <div class="bg-gray-100 p-2 rounded cursor-pointer w-full" @click="copy(item['corrected'])">
+            <span v-text="item['corrected']" />
+            <ul class="list-disc list-inside p-2">
+              <li v-for="explanation in item['explanations']" v-text="explanation" />
+            </ul>
+          </div>
+          <a class="text-blue-500 cursor-pointer" @click="editItem(history.indexOf(item))">Edit</a>
+          <a class="text-red-500 cursor-pointer" @click="deleteItem(history.indexOf(item))">Delete</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
 <script setup lang="ts">
 import { Configuration, OpenAIApi } from "openai";
 import { toast, type ToastOptions } from 'vue3-toastify';
@@ -6,6 +54,7 @@ import { Ref, ref } from 'vue'
 
 const props = defineProps<{
   apiKey: string
+  prompt: string
 }>()
 
 const configuration = new Configuration({
@@ -15,30 +64,38 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const text: Ref<string> = ref('');
-const correctedText: Ref<string> = ref('');
+const temperature: Ref<number>  = ref(1);
+const top_p: Ref<number> = ref(1);
 const responseJson: Ref<any> = ref({});
 const history: Ref<any> = ref([]);
 
 const send = async () => {
-  const response = await openai.createCompletion({
+  try{
+    const prompt = props.prompt.replace('###text###', `${text.value}`);
+    console.log(prompt);
+
+    const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `Correct the spelling and grammar of the following text:
-    '''${text.value}'''
-return a valid json with the keys:
-"corrected": The corrected text
-"explanations": Array of explanations for each correction
-`,
-    temperature: 0,
+    prompt: prompt,
+    temperature: Number(temperature.value),
     max_tokens: 1000,
-    top_p: 1,
+    top_p: Number(top_p.value),
     frequency_penalty: 0,
     presence_penalty: 0,
   });
-
-  responseJson.value = JSON.parse(response.data.choices[0].text ?? '');
+  responseJson.value = JSON.parse(response.data.choices[0].text?.trimStart() ?? '');
   history.value.unshift(responseJson.value);
   savetoStorage();
   navigator.clipboard.writeText(responseJson.value['corrected']);
+  }catch(e : any){
+    console.log(e);
+    const message = e.message ?? e;
+    toast(`${message}`, {
+      autoClose: 1000,
+      delay: 1,
+      position: toast.POSITION.BOTTOM_CENTER,
+    } as ToastOptions);
+  }  
 }
 
 const loadfromStorage = () => {
@@ -79,40 +136,6 @@ const savetoStorage = () => {
 loadfromStorage();
 
 </script>
-
-<template>
-  <div>
-    <div class="flex flex-col max-w-4xl mx-auto">
-      <a class="text-blue-500 cursor-pointer" @click="reset">Clear</a>
-      <div class="flex flex-row space-x-2">
-        <textarea class="border rounded w-full min-h-[160px] p-2" v-model="text" />
-        <div class="border rounded w-full bg-gray-100 p-2 overflow-y-scroll min-h-full">
-          <span v-text="responseJson['corrected']" />
-          <ul class="list-disc list-inside">
-            <li v-for="explanation in responseJson['explanations']" v-text="explanation" />
-          </ul>
-        </div>
-      </div>
-      <br>
-      <button class="btn btn-blue" type="button" @click="send">Process</button>
-      <br>
-      <br>
-      <span class="text-2xl font-extrabold">History</span>
-      <div class="flex flex-col space-y-2">
-        <div v-for="item in history" class="flex items-center space-x-2">
-          <div class="bg-gray-100 p-2 rounded cursor-pointer w-full" @click="copy(item['corrected'])">
-            <span v-text="item['corrected']" />
-            <ul class="list-disc list-inside p-2">
-              <li v-for="explanation in item['explanations']" v-text="explanation" />
-            </ul>
-          </div>
-          <a class="text-blue-500 cursor-pointer" @click="editItem(history.indexOf(item))">Edit</a>
-          <a class="text-red-500 cursor-pointer" @click="deleteItem(history.indexOf(item))">Delete</a>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
 
 <style>
 .btn {
